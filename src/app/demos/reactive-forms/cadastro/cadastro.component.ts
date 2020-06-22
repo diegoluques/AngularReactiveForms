170e51d974f03460096ccbcd78c98027a3e326e0
Diego <luquesdiego@outlook.com>
2020-06-21 20:56:29 -0400

    Validações avançadas de formulário

--- src/app/demos/reactive-forms/cadastro/cadastro.component.ts
+++ src/app/demos/reactive-forms/cadastro/cadastro.component.ts
@@ -1,23 +1,61 @@
-import { Usuario } from './models/usuario';
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Observable, fromEvent, merge } from 'rxjs';
+import { Component, OnInit, AfterViewInit, ViewChildren, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, FormControlName } from '@angular/forms';
+
 import { __decorate } from "tslib";
+import { Usuario } from './models/usuario';
 import { NgBrazilValidators } from 'ng-brazil';
 import { utilsBr } from 'js-brasil';
 import { CustomValidators } from 'ng2-validation';
+import { ValidationMessages, GenericValidator, DisplayMessage } from './generic-form-validations';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html'
 })
-export class CadastroComponent implements OnInit {
+export class CadastroComponent implements OnInit, AfterViewInit {
+
+  @ViewChildren(FormControlName, { read: ElementRef }) FormInputElements: ElementRef[];
 
   cadastroForm: FormGroup;
   usuario: Usuario;
   formResult: string = "";
-  masks = utilsBr.MASKS;
+  mask = utilsBr.MASKS;
 
-  constructor(private fb: FormBuilder) { }
+  validationMessages: ValidationMessages;
+  genericValidator: GenericValidator;
+  displayMessage: DisplayMessage = {}
+
+  constructor(private fb: FormBuilder) {
+
+    this.validationMessages = {
+      nome: {
+        required: 'Nome é obrigatório',
+        minlength: 'O Nome precisa ter no mínimo 2 caracteres',
+        maxlength: 'O Nome precisa ter no máximo 150 caracteres'
+      },
+      cpf: {
+        required: 'CPF é obrigatório',
+        cpf: 'CPF inválido'
+      },
+      email: {
+        required: 'E-mail é obrigatório',
+        email: 'E-mail inválido'
+      },
+      senha: {
+        required: 'Senha é obrigatório',
+        rangeLength: 'A Senha deve possuir entre 6 e 15 caracteres'
+      },
+      confirmeSenha: {
+        required: 'Confirme a Senha é obrigatório',
+        rangeLength: 'A Senha deve possuir entre 6 e 15 caracteres',
+        equalTo: 'As senhas não conferem'
+      }
+    };
+
+    this.genericValidator = new GenericValidator(this.validationMessages);
+
+  }
 
   ngOnInit(): void {
 
@@ -30,14 +68,27 @@ export class CadastroComponent implements OnInit {
       ]
     );
 
-    this.cadastroForm = this.fb.group({
-      nome: ['', Validators.required],
+    this.cadastroForm = this.fb.group({ 
+      nome: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(150)]],
       cpf: ['', [Validators.required, NgBrazilValidators.cpf]],
       email: ['', [Validators.required, Validators.email]],
       senha: senha,
       confirmeSenha: confirmeSenha
     })
 
+  }
+
+  ngAfterViewInit() {
+
+    //Cria uma coleção de observables que são disparadas através do evento blur for disparado
+    let controlBlurs: Observable<any>[] = this.FormInputElements
+      .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
+
+    //Pega todas as coleções de observables e faz o subscribe para todas 
+    merge(...controlBlurs).subscribe(() => {
+      //Subscribe é chamda toda vez que perde o foco do formuláio (input)
+      this.displayMessage = this.genericValidator.processarMensagens(this.cadastroForm);
+    })
   }
 
   adicionarUsuario() {
